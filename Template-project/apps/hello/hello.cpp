67e9c64a41756735e508504b38bc06383ee173f7
Fabio Pellacini <fabio.pellacini@gmail.com>
2021-09-10 11:42:03 +0200

    updated

--- Template-project/apps/hello/hello.cpp
+++ Template-project/apps/hello/hello.cpp
@@ -4,7 +4,9 @@
 int main() {
   glfwInit();
   auto window = glfwCreateWindow(521, 521, "ciao Enrico", nullptr, nullptr);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  glfwMakeContextCurrent(window);
+  glfwShowWindow(window);
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   glfwDestroyWindow(window);
   glfwTerminate();
 }
